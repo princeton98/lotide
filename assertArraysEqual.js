afc3ca6f2b7b900e8102bf95c153ba09a0d21afe
Princeton Arulnesan <princeton.arul@gmail.com>
2020-04-28 23:21:18 +0000

    made changes to eq, and finished up assertArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`😤😤😤 Assertion Passed: ${actual} === ${expected}`);
@@ -17,11 +19,21 @@ const eqArrays = function (array1, array2) {
     }
   }
   if (counter === array1.length) {
-    return true
+    return true;
   } else return false;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`😤😤😤 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🤮🤮🤮 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+assertArraysEqual([1, 2, 2], [1, 2, 2]);
+
+console.log(eqArrays([1, 4, 2], [5, 4, 2]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
