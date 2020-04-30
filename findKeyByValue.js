eb19bad1f3e0b2f8c9586a81a4e1a80804a7e24d
Princeton Arulnesan <princeton.arul@gmail.com>
2020-04-30 16:26:45 +0000

    added a function to find key with value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😤😤😤 Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`🤮🤮🤮 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(input, value) {
+  // scans object to find value
+  // to get value, need key
+  newInput = Object.keys(input)
+  for (let key of newInput) {
+    // console.log(input[key]);
+    if (input[key] === value) {
+      return key;
+    }
+    
+    // use key to find value
+  } 
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
