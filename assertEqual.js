8a86070234a5cc2f74bf815579456969d3891f3b
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-04 16:37:02 +0000

    added tests for head and assert, refactored code for assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -4,5 +4,5 @@ const assertEqual = function(actual, expected) {
   } else
     console.log(`🤮🤮🤮 Assertion Failed: ${actual} !== ${expected}`);
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
+
+module.exports = assertEqual;
